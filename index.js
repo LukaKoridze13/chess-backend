f0c40d4f626534d67eac4fac2ddef6054ce4c4fc
Luka Koridze <lukakoridze13@gmail.com>
2023-05-22 14:58:55 +0400

    Update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.use("api/accounts", AccountRequests);
+app.use("/api/accounts", AccountRequests);
 
 app.get("/", (req, res) => {
   res.send("Server is running successfully");
